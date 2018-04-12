338d6cdba92662c187c1206e9f1c6809e21819bd
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2018-04-12 09:46:26 +0200

    Reproducing of unexpected exception just in the constructor call

--- macros/src/test/scala/io/crossref/CrossrefSpec.scala
+++ macros/src/test/scala/io/crossref/CrossrefSpec.scala
@@ -4,6 +4,62 @@ import com.github.plokhotnyuk.jsoniter_scala.core._
 import org.scalatest.{Matchers, WordSpec}
 
 class CrossrefSpec extends WordSpec with Matchers {
+  "Publication" should {
+    "be created by the constructor call" in {
+      new io.crossref.Publication(
+        publisher = "",
+        title = Nil,
+        `original-title` = Nil,
+        `short-title` = Nil,
+        `abstract` = None,
+        `reference-count` = 1,
+        `references-count` = 1,
+        `is-referenced-by-count` = 1,
+        source = "",
+        prefix = "",
+        DOI = "",
+        URL = "",
+        member = "",
+        `type` = "",
+        created = null,
+        deposited = null,
+        indexed = null,
+        issued = null,
+        posted = None,
+        accepted = None,
+        subtitle = Nil,
+        `container-title` = Nil,
+        `short-container-title` = Nil,
+        `group-title` = None,
+        issue = None,
+        volume = None,
+        page = None,
+        `article-number` = None,
+        `published-print` = None,
+        `published-online` = None,
+        subject = Nil,
+        ISSN = Nil,
+        `issn-type` = Nil,
+        ISBN = Nil,
+        archive = Nil,
+        license = Nil,
+        funder = Nil,
+        assertion = Nil,
+        author = Nil,
+        editor = Nil,
+        chair = Nil,
+        translator = Nil,
+        `update-to` = Nil,
+        `update-policy` = None,
+        link = Nil,
+        `clinical-trial-number` = Nil,
+        `alternative-id` = None,
+        reference = Nil,
+        `content-domain` = None,
+        relation = None,
+        score = 0.0)
+    }
+  }
   "JsonImplicits" should {
     "parse data.json from file" in {
       val data = readFromStream(getClass.getResourceAsStream("data.json"))(JsonImplicits.codec)
