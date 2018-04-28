70d4243852d97005b6a461b4fe9b4b93b2c48fd2
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2018-04-28 06:38:09 +0200

    An advanced bench for GraalVM

--- benchmark/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/macros/AdtBenchmark.scala
+++ benchmark/src/main/scala/com/github/plokhotnyuk/jsoniter_scala/macros/AdtBenchmark.scala
@@ -1,6 +1,7 @@
 package com.github.plokhotnyuk.jsoniter_scala.macros
 
 import java.nio.charset.StandardCharsets._
+import java.util.concurrent.atomic.AtomicReferenceArray
 
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type
 import com.fasterxml.jackson.annotation.{JsonSubTypes, JsonTypeInfo}
@@ -32,31 +33,79 @@ class AdtBenchmark extends CommonParams {
   val jsonString: String = """{"type":"C","l":{"type":"A","a":1},"r":{"type":"B","b":"VVV"}}"""
   val jsonString2: String = """{"l":{"a":1,"type":"A"},"r":{"b":"VVV","type":"B"},"type":"C"}"""
   val jsonBytes: Array[Byte] = jsonString.getBytes(UTF_8)
+  val objQueue: AtomicReferenceArray[AdtBase] = new AtomicReferenceArray(128)
+  val jsonQueue: AtomicReferenceArray[Array[Byte]] = new AtomicReferenceArray(128)
+
+  @volatile var i = 0
 
   @Benchmark
-  def readCirce(): AdtBase = decode[AdtBase](new String(jsonBytes, UTF_8)).fold(throw _, x => x)
+  def readCirce(): AdtBase = {
+    val x = decode[AdtBase](new String(jsonBytes, UTF_8)).fold(throw _, x => x)
+    objQueue.lazySet(i & 127, x)
+    i += 1
+    x
+  }
 
   @Benchmark
-  def readJacksonScala(): AdtBase = jacksonMapper.readValue[AdtBase](jsonBytes)
+  def readJacksonScala(): AdtBase = {
+    val x = jacksonMapper.readValue[AdtBase](jsonBytes)
+    objQueue.lazySet(i & 127, x)
+    i += 1
+    x
+  }
 
   @Benchmark
-  def readJsoniterScala(): AdtBase = readFromArray[AdtBase](jsonBytes)
+  def readJsoniterScala(): AdtBase = {
+    val x = readFromArray[AdtBase](jsonBytes)
+    objQueue.lazySet(i & 127, x)
+    i += 1
+    x
+  }
 
   @Benchmark
-  def readPlayJson(): AdtBase = Json.parse(jsonBytes).as[AdtBase](adtFormat)
+  def readPlayJson(): AdtBase = {
+    val x = Json.parse(jsonBytes).as[AdtBase](adtFormat)
+    objQueue.lazySet(i & 127, x)
+    i += 1
+    x
+  }
 
   @Benchmark
-  def writeCirce(): Array[Byte] = printer.pretty(obj.asJson).getBytes(UTF_8)
+  def writeCirce(): Array[Byte] = {
+    val x = printer.pretty(obj.asJson).getBytes(UTF_8)
+    jsonQueue.lazySet(i & 127, x)
+    i += 1
+    x
+  }
 
   @Benchmark
-  def writeJacksonScala(): Array[Byte] = jacksonMapper.writeValueAsBytes(obj)
+  def writeJacksonScala(): Array[Byte] = {
+    val x = jacksonMapper.writeValueAsBytes(obj)
+    jsonQueue.lazySet(i & 127, x)
+    i += 1
+    x
+  }
 
   @Benchmark
-  def writeJsoniterScala(): Array[Byte] = writeToArray(obj)
+  def writeJsoniterScala(): Array[Byte] = {
+    val x = writeToArray(obj)
+    jsonQueue.lazySet(i & 127, x)
+    i += 1
+    x
+  }
 
   @Benchmark
-  def writeJsoniterScalaPrealloc(): Int = writeToPreallocatedArray(obj, preallocatedBuf, preallocatedOff)
+  def writeJsoniterScalaPrealloc(): Int = {
+    val x = writeToPreallocatedArray(obj, preallocatedBuf, preallocatedOff)
+    i ^= preallocatedBuf(i & 127)
+    x
+  }
 
   @Benchmark
-  def writePlayJson(): Array[Byte] = Json.toBytes(Json.toJson(obj)(adtFormat))
+  def writePlayJson(): Array[Byte] = {
+    val x = Json.toBytes(Json.toJson(obj)(adtFormat))
+    jsonQueue.lazySet(i & 127, x)
+    i += 1
+    x
+  }
 }
